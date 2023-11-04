9e4f7c03d340a4105b5ba12ed7963eaedfb1bf0b
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-11-04 23:30:09 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { sources } = require("next/dist/compiled/webpack/webpack");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+
+  rewrites: async () => [
+    {
+      source: "/api/:path*",
+      destination: `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/:path*`,
+    },
+  ],
+};
+
+module.exports = nextConfig;
