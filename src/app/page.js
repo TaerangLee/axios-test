7e51bfb6be0a1168d92350acf172cab85ec07c55
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-11-05 23:08:41 +0900

    Update page settings

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./page.module.css";
+import Main from "@/components/Main";
 
 export default function Home() {
-  return <div>asd</div>;
+  return <Main />;
 }
